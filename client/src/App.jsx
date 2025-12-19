a047091682b4642b1bde342c5ba63c225dcb685d
ThaiBao <tiboogamerk3@gmail.com>
2025-12-19 16:52:14 +0700

    Cai React/Vite/Tailwind v12.19

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    // Thử nghiệm màu nền đen, chữ vàng (đúng gu của bạn)
+    <div class="bg-[url(./imgs/Anh-1.png)] bg-cover bg-fixed bg-center w-full h-screen flex justify-center items-center">
+        <h1 class="text-[120px] font-extrabold text-transparent bg-clip-text bg-[url(./imgs/Anh-2.png)] bg-fixed bg-cover bg-center">HELLO TAILWIND</h1>
+        <div class="h-100vh"></div>
+    </div>
+  )
+}
+
+export default App
